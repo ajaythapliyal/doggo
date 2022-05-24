5607a25b54297390388e23d24577125b17fe6490
ajaythapliyal <ajaythapliyal1703@gmail.com>
2022-05-24 09:23:04 +0530

    sets up infinite scroll

--- dog-service.js
+++ dog-service.js
@@ -19,16 +19,49 @@ export async function fetchDogs(number=50){
         throw Error("Fetching dogs failed")
     }
 
-    // const dogs = await new Promise((resolve, reject)=>{
+
+    //     const dogs = await new Promise((resolve, reject)=>{
     //     setTimeout(()=>{
-    //         resolve([
-    //             "https://images.dog.ceo/breeds/spaniel-brittany/n02101388_5512.jpg",
-    //             "https://images.dog.ceo/breeds/briard/n02105251_5985.jpg",
-    //             "https://images.dog.ceo/breeds/dachshund/dog-2643027_640.jpg",
-    //             "https://images.dog.ceo/breeds/terrier-fox/n02095314_2863.jpg",
-    //             "https://images.dog.ceo/breeds/terrier-american/n02093428_4939.jpg",
-    //             "https://images.dog.ceo/breeds/terrier-scottish/n02097298_12063.jpg"
-    //         ])
+    //         resolve({ message :
+    //             [
+    //                 "https://images.dog.ceo/breeds/spaniel-brittany/n02101388_5512.jpg",
+    //                 "https://images.dog.ceo/breeds/briard/n02105251_5985.jpg",
+    //                 "https://images.dog.ceo/breeds/dachshund/dog-2643027_640.jpg",
+    //                 "https://images.dog.ceo/breeds/terrier-fox/n02095314_2863.jpg",
+    //                 "https://images.dog.ceo/breeds/terrier-american/n02093428_4939.jpg",
+    //                 "https://images.dog.ceo/breeds/terrier-scottish/n02097298_12063.jpg",
+    //                 "https://images.dog.ceo/breeds/spaniel-brittany/n02101388_5512.jpg",
+    //                 "https://images.dog.ceo/breeds/briard/n02105251_5985.jpg",
+    //                 "https://images.dog.ceo/breeds/dachshund/dog-2643027_640.jpg",
+    //                 "https://images.dog.ceo/breeds/terrier-fox/n02095314_2863.jpg",
+    //                 "https://images.dog.ceo/breeds/terrier-american/n02093428_4939.jpg",
+    //                 "https://images.dog.ceo/breeds/terrier-scottish/n02097298_12063.jpg",
+    //                 "https://images.dog.ceo/breeds/spaniel-brittany/n02101388_5512.jpg",
+    //                 "https://images.dog.ceo/breeds/briard/n02105251_5985.jpg",
+    //                 "https://images.dog.ceo/breeds/dachshund/dog-2643027_640.jpg",
+    //                 "https://images.dog.ceo/breeds/terrier-fox/n02095314_2863.jpg",
+    //                 "https://images.dog.ceo/breeds/terrier-american/n02093428_4939.jpg",
+    //                 "https://images.dog.ceo/breeds/terrier-scottish/n02097298_12063.jpg",
+    //                 "https://images.dog.ceo/breeds/spaniel-brittany/n02101388_5512.jpg",
+    //                 "https://images.dog.ceo/breeds/briard/n02105251_5985.jpg",
+    //                 "https://images.dog.ceo/breeds/dachshund/dog-2643027_640.jpg",
+    //                 "https://images.dog.ceo/breeds/terrier-fox/n02095314_2863.jpg",
+    //                 "https://images.dog.ceo/breeds/terrier-american/n02093428_4939.jpg",
+    //                 "https://images.dog.ceo/breeds/terrier-scottish/n02097298_12063.jpg",
+    //                 "https://images.dog.ceo/breeds/spaniel-brittany/n02101388_5512.jpg",
+    //                 "https://images.dog.ceo/breeds/briard/n02105251_5985.jpg",
+    //                 "https://images.dog.ceo/breeds/dachshund/dog-2643027_640.jpg",
+    //                 "https://images.dog.ceo/breeds/terrier-fox/n02095314_2863.jpg",
+    //                 "https://images.dog.ceo/breeds/terrier-american/n02093428_4939.jpg",
+    //                 "https://images.dog.ceo/breeds/terrier-scottish/n02097298_12063.jpg",
+    //                 "https://images.dog.ceo/breeds/spaniel-brittany/n02101388_5512.jpg",
+    //                 "https://images.dog.ceo/breeds/briard/n02105251_5985.jpg",
+    //                 "https://images.dog.ceo/breeds/dachshund/dog-2643027_640.jpg",
+    //                 "https://images.dog.ceo/breeds/terrier-fox/n02095314_2863.jpg",
+    //                 "https://images.dog.ceo/breeds/terrier-american/n02093428_4939.jpg",
+    //                 "https://images.dog.ceo/breeds/terrier-scottish/n02097298_12063.jpg"
+    //             ]
+    //         })
     //     }, 3000)
     // })
 }
--- view.js
+++ view.js
@@ -1,9 +1,29 @@
+import { fetchDogs } from "./dog-service.js";
+import { store } from "./store.js";
+
 const main = document.querySelector("main");
+const body = document.querySelector("body");
 
 export function showSpinner(){
-   main.innerHTML = `<div class="spinner">
-                        <div class="inner-spinner"></div>
-                    </div>`
+   main.innerHTML = getSpinner();
+}
+
+export function appendSpinner(){
+    if(!body.lastElementChild.classList.contains('spinner')){
+        body.innerHTML+=getSpinner();
+        unsetInfiniteScroll();
+    }
+}
+
+export function getSpinner(){
+    return `<div class="spinner">
+                <div class="inner-spinner"></div>
+            </div>`
+}
+
+
+export function removeAllSpinners(){
+    body.querySelectorAll('.spinner').forEach(spinner => spinner.remove())
 }
 
 
@@ -11,10 +31,34 @@ export function render(images){
     if (!images || images.length === 0){
         return showSpinner()
     }
+    removeAllSpinners();
     const imageView =  images.reduce((accu, image)=>{
         return `${accu}
-        <img src=${image} loading="lazy">`
+        <img src="${image}" loading="lazy">`
     }, "")
-    
-    main.innerHTML = imageView
+    document.querySelector("main").innerHTML = imageView;
+    setInfiniteScroll();
+}
+
+
+export function setInfiniteScroll(){
+    window.addEventListener('scroll', infiniteScroll)
+}
+
+export function unsetInfiniteScroll(){
+    window.removeEventListener('scroll', infiniteScroll)
+}
+
+
+function infiniteScroll(){
+    const scrollHeight = Math.max(
+        document.body.scrollHeight, document.documentElement.scrollHeight,
+        document.body.offsetHeight, document.documentElement.offsetHeight,
+        document.body.clientHeight, document.documentElement.clientHeight
+      );
+    const bottomToScroll = scrollHeight - (window.pageYOffset+document.documentElement.clientHeight)
+        if (bottomToScroll < 100){
+            appendSpinner()
+            fetchDogs().then(dogs => store.addDogs(dogs));
+        }
 }
